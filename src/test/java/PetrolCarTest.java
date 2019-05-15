f18658e590f779605960daa4c723f21da7fd98e7
Mateusz <matstanczak@gmail.com>
2019-05-15 14:04:11 +0100

    petrolCarTest pass

--- src/test/java/PetrolCarTest.java
+++ src/test/java/PetrolCarTest.java
@@ -4,6 +4,8 @@ import CarDealership.Vehicles.Tyre;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class PetrolCarTest {
 
     PetrolCar petrolCar;
@@ -14,10 +16,31 @@ public class PetrolCarTest {
     public void setUp() throws Exception {
         engine = new Engine("Petrol");
         tyre = new Tyre("Dunlop");
-        petrolCar = new PetrolCar(engine, tyre, 1000.00, "pink", 0);
+        petrolCar = new PetrolCar(engine, tyre, 1000, "pink", 0);
     }
 
     @Test
-    public void name() {
+    public void hasEngine() {
+        assertEquals(engine, petrolCar.getEngine());
+    }
+
+    @Test
+    public void hasTyre(){
+        assertEquals(tyre, petrolCar.getTyre());
+    }
+
+    @Test
+    public void hasPrice() {
+        assertEquals(1000, petrolCar.getPrice(), 0.01);
+    }
+
+    @Test
+    public void hasColour() {
+        assertEquals("pink", petrolCar.getColour());
+    }
+
+    @Test
+    public void hasDamage() {
+        assertEquals(0, petrolCar.getDamage());
     }
 }
