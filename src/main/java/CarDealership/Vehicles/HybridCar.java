cd09f7b927c8df461885fd6aa4726e5df35e95cd
Mateusz <matstanczak@gmail.com>
2019-05-15 14:45:32 +0100

    hybridCarTest passing

--- src/main/java/CarDealership/Vehicles/HybridCar.java
+++ src/main/java/CarDealership/Vehicles/HybridCar.java
@@ -1,4 +1,8 @@
 package CarDealership.Vehicles;
 
-public class HybridCar {
+public class HybridCar extends Vehicle {
+
+    public HybridCar(Engine engine, Tyre tyre, double price, String colour, int damage) {
+        super(engine, tyre, price, colour, damage);
+    }
 }
--- src/test/java/HybridCarTest.java
+++ src/test/java/HybridCarTest.java
@@ -1,2 +1,46 @@
+import CarDealership.Vehicles.Engine;
+import CarDealership.Vehicles.HybridCar;
+import CarDealership.Vehicles.Tyre;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class HybridCarTest {
+
+    Engine engine;
+    Tyre tyre;
+    HybridCar hybridCar;
+
+    @Before
+    public void setUp() throws Exception {
+        engine = new Engine("hybrid");
+        tyre = new Tyre("Michelin");
+        hybridCar = new HybridCar(engine, tyre, 50000.00, "purple", 0);
+    }
+
+    @Test
+    public void hasEngine() {
+        assertEquals(engine, hybridCar.getEngine());
+    }
+
+    @Test
+    public void hasTyre() {
+        assertEquals(tyre, hybridCar.getTyre());
+    }
+
+    @Test
+    public void hasPrice() {
+        assertEquals(50000.00, hybridCar.getPrice(), 0.01);
+    }
+
+    @Test
+    public void hasColour() {
+        assertEquals("purple", hybridCar.getColour());
+    }
+
+    @Test
+    public void hasDamage() {
+        assertEquals(0, hybridCar.getDamage());
+    }
 }
