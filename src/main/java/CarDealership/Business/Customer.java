ba7667a539afbdeda5a1b15f25efcab33f7b9462
Alasdair Carstairs <alasdaircarstairs@Agnes-MacBook-Pro.local>
2019-05-15 14:14:24 +0100

    customer tested

--- src/main/java/CarDealership/Business/Customer.java
+++ src/main/java/CarDealership/Business/Customer.java
@@ -1,4 +1,43 @@
 package CarDealership.Business;
 
+import CarDealership.Vehicles.Vehicle;
+
 public class Customer {
+    private double money;
+    private String name;
+    private Vehicle vehicle;
+
+    public Customer(double money, String name, Vehicle vehicle) {
+        this.money = money;
+        this.name = name;
+        this.vehicle = vehicle;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void removeVehicle(){
+        vehicle = null;
+    }
+
+    public void takeVehicle(Vehicle vehicle){
+        this.vehicle = vehicle;
+    }
+
+    public void addMoney(double amount){
+        money += amount;
+    }
+
+    public void reduceMoney(double amount){
+        money -= amount;
+    }
 }
--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,2 +1,57 @@
+import CarDealership.Business.Customer;
+import CarDealership.Vehicles.Engine;
+import CarDealership.Vehicles.PetrolCar;
+import CarDealership.Vehicles.Tyre;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CustomerTest {
+
+    Customer customer;
+    PetrolCar petrolCar;
+    Engine engine;
+    Tyre tyre;
+
+    @Before
+    public void setUp() throws Exception {
+        customer = new Customer(5000, "Harry", null);
+        engine = new Engine("Petrol");
+        tyre = new Tyre("Dunlop");
+        petrolCar = new PetrolCar(engine, tyre, 1000, "pink", 0);
+    }
+
+
+    @Test
+    public void hasInfo() {
+        assertEquals(5000, customer.getMoney(),0);
+        assertEquals("Harry", customer.getName());
+        assertEquals(null, customer.getVehicle());
+    }
+
+    @Test
+    public void assignVehicle() {
+        customer.takeVehicle(petrolCar);
+        assertEquals(petrolCar, customer.getVehicle());
+    }
+
+    @Test
+    public void removeVehicle() {
+        customer.takeVehicle(petrolCar);
+        customer.removeVehicle();
+        assertEquals(null, customer.getVehicle());
+    }
+
+    @Test
+    public void increaseCustomerMoney() {
+        customer.addMoney(500);
+        assertEquals(5500, customer.getMoney(),0);
+    }
+
+    @Test
+    public void decreaseCustomerMoney() {
+        customer.reduceMoney(500);
+        assertEquals(4500, customer.getMoney(),0);
+    }
 }
