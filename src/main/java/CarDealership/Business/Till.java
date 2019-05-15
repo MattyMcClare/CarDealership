904465a24b88823b775a3b02613cc95d3c104156
Alasdair Carstairs <alasdaircarstairs@Agnes-MacBook-Pro.local>
2019-05-15 14:25:08 +0100

    till tested

--- src/main/java/CarDealership/Business/Till.java
+++ src/main/java/CarDealership/Business/Till.java
@@ -1,4 +1,21 @@
 package CarDealership.Business;
 
 public class Till {
+    private double balance;
+
+    public Till(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void increaseBalance(double balance) {
+        this.balance += balance;
+    }
+
+    public void decreaseBalance(double balance) {
+        this.balance -= balance;
+    }
 }
--- src/test/java/TillTest.java
+++ src/test/java/TillTest.java
@@ -1,2 +1,32 @@
+import CarDealership.Business.Till;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TillTest {
+
+    Till till;
+
+    @Before
+    public void setUp() throws Exception {
+        till = new Till(10000);
+    }
+
+    @Test
+    public void hasBalance() {
+        assertEquals(10000, till.getBalance(),0);
+    }
+
+    @Test
+    public void increaseBalance() {
+        till.increaseBalance(1000);
+        assertEquals(11000, till.getBalance(),0);
+    }
+
+    @Test
+    public void decreaseBalance() {
+        till.decreaseBalance(1000);
+        assertEquals(9000, till.getBalance(),0);
+    }
 }
